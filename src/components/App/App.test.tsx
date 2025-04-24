0172f6505ba666b1cc4fe3965334fb3385dcb0f3
Jack Westbrook <jack.westbrook@gmail.com>
2025-04-24 10:01:14 +0200

    Wip

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { AppRootProps, PluginType } from '@grafana/data';
 import { render, screen } from '@testing-library/react';
 import App from './App';
